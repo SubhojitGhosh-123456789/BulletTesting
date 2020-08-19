ee447b10199b8c91104e1e5bd4db6c5746019a81
SubhojitGhosh-123456789 <69459860+SubhojitGhosh-123456789@users.noreply.github.com>
2020-08-19 18:13:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
   createCanvas(1600,400);
   
   bullet = createSprite(50,200,100,20);
-  bullet.shapeColor="white";
+  bullet.shapeColor="green";
 
   wall = createSprite(1200,200,thickness,height/2);
   wall.shapeColor=color(80,80,80);
